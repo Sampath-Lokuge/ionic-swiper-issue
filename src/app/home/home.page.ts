bad16845b328baccfe12057fd4394bfb36bc2069
Sampath-Lokuge <sugeesampathlokuge@gmail.com>
2023-05-01 19:35:40 +0530

    swiper issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { IonicSlides } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  swiperModules = [IonicSlides];
 
   constructor() {}
-
 }
